8cefb6e718fa9a4b79f3eaa209d1a81b7ac5adb0
Ibrahim Abedghane <abedghaneibrahim94@gmail.com>
2024-04-22 22:19:42 +0000

    added 'setupGame' function to switch between the game modes

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,3 +12,9 @@ const choicesConfig = {
     classic: ['rock', 'paper', 'scissors'],
     spock: ['rock', 'paper', 'scissors', 'lizard', 'spock']
 };
+
+window.setupGame = function() {
+    gameState.gameMode = mode;
+    document.getElementById('selectionScreen').classList.add('hidden');
+    document.getElementById('usernameScreen').classList.remove('hidden');
+};
