581732940b5ad5cf25bb16164124673562050f62
Ibrahim Abedghane <abedghaneibrahim94@gmail.com>
2024-04-22 22:09:31 +0000

    added JavaScript to set up initial state for the game to ensure that the script runs after doc is ready

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded',function() {
+    const gameState = {
+        playerScore: 0,
+        computerScore: 0,
+        draws: 0,
+        gameMode: null,
+        username: 'Player'
+    }; 
+});
